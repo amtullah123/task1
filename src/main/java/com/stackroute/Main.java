63db2ef6ae7c6c15469deaa8e8cff933ccf6aa12
amtullah123 <amtullah123@gmail.com>
2019-05-24 12:24:51 +0530

    testing with singleton

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,34 +1,17 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class Main
 {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie= (Movie) context.getBean("movie");
-        System.out.println(movie.getActor().getName()+ " acted in " + movie.getMName());
 
-        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie3=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
-        System.out.println("Actor details using BeanDefinitionRegistry:\n"+movie3);
-
-        ClassPathResource resource= new ClassPathResource("beans.xml");
-        BeanFactory beanFactory=new XmlBeanFactory(resource);
-        Movie movie1=beanFactory.getBean("movie",Movie.class);
-        System.out.println("Actor details using BeanFactory:\n"+movie1+"\n");
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie4= (Movie) context1.getBean("movie");
+        Movie movie5= (Movie) context1.getBean("movie1");
+        System.out.println(movie4==movie5);
     }
 }
 
